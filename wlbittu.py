d6544e0e3336a5e261f0193e69c85b8a6bb1a52e
updatedaily87 <update9101@gmail.com>
2025-06-27 02:45:13 +0530

    wlbittu.py

--- wlbittu.py
+++ wlbittu.py
@@ -82,7 +82,7 @@ async def like_handler(msg: Message):
         return
 
     wait = await msg.reply("⏳ Likes bhej rahe hain, rukko....")
-    url = f"https://wlyousuf.onrender.com/like?uid={uid}&region={region.lower()}&key=wlbittu"
+    url = f"https://wlbittu-bot-j3w0.onrender.com/like?uid={uid}&region={region.lower()}&key=wlbittu"
     data = await fetch_json(url)
 
     if not data:
